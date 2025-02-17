2e1e337c5aeae7aee39278bc3f401b6e8b2ac803
Rino Arif Budiyanto <rino.asuszen2@gmail.com>
2025-02-17 12:20:24 +0700

    update cv new

--- app/hero/hero.jsx
+++ app/hero/hero.jsx
@@ -47,7 +47,7 @@ const Hero = () => {
               spirit!
             </p>
             <a
-              href="CVRino.pdf"
+              href="CvRinoA.pdf"
               target="_blank"
               className="bg-cyan-500 p-3 rounded-full mt-4 inline-block text-center text-white hover:bg-cyan-400 transition duration-300"
             >
